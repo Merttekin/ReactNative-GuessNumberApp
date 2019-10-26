edc1ac06f25b504d445e31f3fb8ef3b60687bb95
Merttekin <merttekin94@gmail.com>
2019-10-27 00:20:52 +0300

    added components

--- App.js
+++ App.js
@@ -6,11 +6,13 @@ import {
 } from 'react-native';
 
 import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 };
